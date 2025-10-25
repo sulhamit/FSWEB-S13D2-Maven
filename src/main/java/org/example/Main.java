ae0aeb2ea389b94677b0c54b60c0500cbe055bde
sulhamit <sulhamit@gmail.com>
2025-10-25 02:20:01 +0200

    isPalindrome(sayinin tersiyle esit olma durumu) tamamlandi.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static Boolean isPalindrome (int b){
+        int a = Math.abs(b);
+
+        int tersSayi =0;
+           while(a!=0){
+               int kalan = a%10;
+               tersSayi = tersSayi*10+kalan;
+                a = a /10;
+           }
+           return tersSayi == Math.abs(b);
+
+    }
+
+
 }
