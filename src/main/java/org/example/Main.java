a6d69ba24c7326958f956cf464aa5f17b35fff45
sulhamit <sulhamit@gmail.com>
2025-10-27 10:17:15 +0100

    gorev 3
    Gorev 3- sayilari ingilizce harflere cevirme tamamlandi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static Boolean isPalindrome (int b){
         int a = Math.abs(b);
-
         int tersSayi =0;
            while(a!=0){
                int kalan = a%10;
@@ -30,8 +29,58 @@ public class Main {
         }
         return toplam == sayi;
     }
+    public static String numberToWords (int sayi){
+        if(sayi<0){
+            return "Invalid Value";
+        }
+        String toWords="";
+        String sayiString = String.valueOf(sayi);
+        for(int i = 0; i<sayiString.length(); i++){
+            char rakam = sayiString.charAt(i);
 
 
+        switch(rakam){
+            case '0':
+              toWords += "Zero ";
+                break;
+
+            case '1':
+                toWords += "One ";
+                break;
+            case '2':
+                toWords += "Two ";
+                break;
+            case '3':
+                toWords += "Three ";
+                break;
+            case '4':
+                toWords += "Four ";
+                break;
+            case '5':
+                toWords += "Five ";
+                break;
+            case '6':
+                toWords += "Six ";
+                break;
+            case '7':
+                toWords += "Seven ";
+                break;
+            case '8':
+                toWords += "Eight ";
+                break;
+            case '9':
+                toWords += "Nine ";
+                break;
+            default:
+                return "Invalid value";
+
+        }
+        }
+
+
+        return toWords.trim();
+
+    }
 
 
 }
